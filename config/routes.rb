57867a8717f7f6b2c3f935f55460ba52e7e9fae5
堂福大成 <>
2017-11-27 19:15:09 +0900

    add root to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'memos/index'
+  root "memos#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
